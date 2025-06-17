8e96c846280f66be64a0b4eb6a96d1560e8b800d
jgtolentino <jgtolentino_rn@yahoo.com>
2025-06-18 04:14:30 +0800

    fix: remove unused useState import

---
+++ scout-analytics-nextjs/.next/static/chunks/app/ai-assist/page-488391c87f597b81.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[423,571,831],{5148:(e,s,n)=>{Promise.resolve().then(n.bind(n,4431)),Promise.resolve().then(n.bind(n,2106))}},e=>{var s=s=>e(e.s=s);e.O(0,[255,244,347,870,358],()=>s(5148)),_N_E=e.O()}]);
--- scout-analytics-nextjs/src/app/page.tsx
+++ scout-analytics-nextjs/src/app/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { useState } from 'react'
 import Header from '../components/Header'
 import Navigation from '../components/Navigation'
 import ExecutiveSummary from '../components/ExecutiveSummary'
